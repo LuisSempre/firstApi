7912e2fb27f2cc51775fcdce5f029c8fc3bcec8b
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-30 17:52:48 -0300

    Add WHATWG URL API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 const http = require('http')
+const { URL } = require('url')
 
 const routes = require('./routes')
 
+
 const server = http.createServer((request, response) => {
-	console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+	const parsedUrl = new URL (`http://localhost:3000${request.url}`)
+	
+	console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
 	const route = routes.find((routeObj) => (
-		routeObj.endpoint === request.url && routeObj.method === request.method
+		routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
 	))
+	
 
 	if (route) {
+		request.query = Object.fromEntries(parsedUrl.searchParams)
+
 		route.handler(request, response)
 	} else {
-		response.writeHead(404, { 'Content-Type': 'text/html' })
-		response.end(`Cannot ${request.method} ${request.url}`)
+		// If no route matches, respond with a 404 error
+		response.writeHead(404, { 'Content-Type': 'text/html'})
+		response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
 	}
+	
 })
 
 server.listen(3000, () => console.log('Server at http://localhost:3000'))
